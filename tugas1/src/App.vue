81eb8fba483026cbd33ab3a141ce9a5d498c8937
syronesdreamland <alifadityaforwork@gmail.com>
2024-06-12 18:01:05 +0700

    first commit

---
+++ tugas1/src/App.vue
@@ -0,0 +1,67 @@
+<script setup>
+import Biodata from './components/Biodata.vue'
+</script>
+
+<template>
+  <div class="app-container">
+    <div class="personal-photo-container">
+      <img src="./assets/10.jpeg" class="personal-photo" alt="Personal Photo" />
+    </div>
+  
+  <Biodata 
+      name="Naufal Arief Faisal" 
+      age="22" 
+      location="Pekanbaru, Riau" 
+      profession="Mahasiswa" 
+      Hobies="Tenis Meja"
+    />
+  </div>
+</template>
+
+<style scoped>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 10vh;
+  background: linear-gradient(135deg, #ffa4a4, #9bcdff, #d1e8ff);
+  padding: 100px;
+  box-sizing: border-box;
+}
+
+
+.personal-photo {
+  height: 150px;
+  width: 150px;
+  border-radius: 50%;
+  object-fit: cover;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+}
+
+.personal-photo:hover {
+  transform: scale(1.5);
+  box-shadow: 0 0 15px rgba(36, 38, 37, 0.7);
+}
+
+.biodata {
+  background: #ffa0a0;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  text-align: center;
+  max-width: 300px;
+}
+
+.biodata h2 {
+  margin: 0;
+  color: #ffffff;
+  font-size: 24px;
+}
+
+.biodata p {
+  margin: 10px 0;
+  font-size: 16px;
+  color: #000000;
+}
+</style>
---
+++ tugas1/src/components/Biodata.vue
@@ -0,0 +1,40 @@
+<!-- src/components/Biodata.vue -->
+<template>
+    <div class="biodata">
+      <h2>{{ name }}</h2>
+      <p><strong>Age:</strong> {{ age }}</p>
+      <p><strong>Location:</strong> {{ location }}</p>
+      <p><strong>Profession:</strong> {{ profession }}</p>
+      <p><strong>Hobies:</strong> {{ Hobies }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Biodata',
+    props: {
+      name: String,
+      age: Number,
+      location: String,
+      profession: String,
+      Hobies: String,
+      SocialMedia: String
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .biodata {
+    font-family: Arial, sans-serif;
+    border: 1px solid #ddd;
+    padding: 1em;
+    border-radius: 8px;
+    max-width: 300px;
+    margin: auto;
+  }
+  .biodata h2 {
+    margin: 0;
+    color: #ffffff;
+  }
+  </style>
+  
